45d2073d02dbc0acb3df99de62ff72c8a2be7f7d
joncoronel <59675970+joncoronel@users.noreply.github.com>
2026-01-27 23:46:19 -0800

    refactor(radio-group): enhance styling and structure for improved user experience
    - Updated RadioGroupItem styles for better visual consistency and accessibility, including refined transition effects.
    - Improved the layout and structure of Radio.Indicator to enhance interaction feedback and maintain a cohesive design.
    - Adjusted class names and added new utility classes for better styling control and responsiveness.

--- registry/default/radio-group/radio-group.tsx
+++ registry/default/radio-group/radio-group.tsx
@@ -25,19 +25,24 @@ function RadioGroupItem({
     <Radio.Root
       data-slot="radio-group-item"
       className={cn(
-        "group relative inline-flex size-4 shrink-0 cursor-pointer items-center justify-center rounded-full",
-        "border-border/80 bg-input border-1 shadow-[0_1px_2px_0_oklch(0.18_0_0_/_0.08)]",
-        "data-checked:border-primary data-checked:bg-primary",
-        "transition-colors duration-200",
-        "focus-visible:outline-ring/50 outline-0 outline-offset-0 outline-transparent transition-[outline-width,outline-offset,outline-color] duration-100 ease-out outline-solid focus-visible:outline-2 focus-visible:outline-offset-2",
+        "group relative inline-flex size-4 shrink-0 items-center justify-center rounded-full",
+        "focus-visible:outline-ring/50 ease-out-cubic outline-0 outline-offset-0 outline-transparent transition-[outline-width,outline-offset,outline-color] duration-150 outline-solid focus-visible:outline-2 focus-visible:outline-offset-2",
         "disabled:cursor-not-allowed disabled:opacity-60",
         "aria-invalid:outline-destructive/50 aria-invalid:outline-2 aria-invalid:outline-offset-2 aria-invalid:outline-solid",
+
+        "before:bg-card before:absolute before:size-full before:rounded-full before:border before:bg-clip-padding before:content-['']",
+        "after:shadow-inset dark:after:shadow-inset-highlight after:absolute after:inset-px after:rounded-full after:content-['']",
 
         className,
       )}
       {...props}
     >
-      <Radio.Indicator className="before:bg-primary-foreground before:animate-in before:zoom-in-50 flex items-center justify-center before:size-2 before:rounded-full before:duration-200" />
+      <Radio.Indicator
+        keepMounted
+        className={cn(
+          "ease-out-cubic before:bg-primary-foreground bg-primary before:ease-out-cubic z-1 flex size-full items-center justify-center rounded-full transition-opacity duration-200 before:size-full before:origin-center before:rounded-full before:transition-[scale] before:duration-200 in-data-checked:before:scale-50 data-checked:opacity-100 data-unchecked:opacity-0",
+        )}
+      />
     </Radio.Root>
   );
 }
